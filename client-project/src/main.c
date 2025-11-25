f0c078b442bb321e60751bdfb2dc34a3197cd54c
Cile04 <j.bravoriveros@studenti.uniba.it>
2025-11-25 21:41:20 +0100

    AGGIORNATO CLIENT E SERVER
    Ristrutturato protocol.h sia nel client che nel server per unificare codici di stato, dimensioni del buffer e strutture dati. Aggiornato il main.c del client per gestire gli argomenti da riga di comando, migliorata la gestione degli errori e implementata la logica di richiesta/risposta per le interrogazioni meteo. Modificata la porta predefinita del server a 56700 e aggiunto il supporto per IP e porta del server configurabili.

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -25,58 +25,311 @@
 #include "protocol.h"
 
 #define NO_ERROR 0
+<<<<<<< Updated upstream
+=======
+#define DEFAULT_IP_SV "127.0.0.1"
 
-int main(int argc, char *argv[]) {
+void clearwinsock()
+{
+#if defined WIN32
+    WSACleanup();
+#endif
+}
 
+void errorhandler(char *error_message)
+{
+    printf("%s", error_message);
+<<<<<<< Updated upstream
+}
+>>>>>>> Stashed changes
+
+char *risposta_meteo(char tipo, float value)
+{
+    static char temp[40];
+    switch (tipo)
+    {
+    case 't':
+        snprintf(temp, sizeof(temp), "Temperatura = %.1f \u00B0C", value);
+        break;
+    case 'p':
+        snprintf(temp, sizeof(temp), "Pressione = %.1f hPA", value);
+        break;
+    case 'h':
+        snprintf(temp, sizeof(temp), "Umidità = %.1f %%", value);
+        break;
+    case 'w':
+        snprintf(temp, sizeof(temp), "Vento: %.1f KM/H", value);
+        break;
+    }
+    return temp;
+}
+
+int main(int argc, char *argv[])
+{
+=======
+}
+
+char *risposta_meteo(char tipo, float value)
+{
+    static char temp[40];
+    switch (tipo)
+    {
+    case 't':
+        snprintf(temp, sizeof(temp), "Temperatura = %.1f \u00B0C", value);
+        break;
+    case 'p':
+        snprintf(temp, sizeof(temp), "Pressione = %.1f hPA", value);
+        break;
+    case 'h':
+        snprintf(temp, sizeof(temp), "Umidità = %.1f %%", value);
+        break;
+    case 'w':
+        snprintf(temp, sizeof(temp), "Vento: %.1f KM/H", value);
+        break;
+    }
+    return temp;
+}
+
+int main(int argc, char *argv[])
+{
+
+    char IP_SV[64] = DEFAULT_IP_SV;
+    int port = SERVER_PORT;
+    weather_request_t wrichiesta;
+
+    // PER CONTROLLARE CHE I PARAMETRI SIANO ESATTAMENTE 3, ALTRIMENTI, ERRORE //
+    if (argc < 3)
+    {
+        fprintf(stderr, "Il formato per l'utilizzo è: %s [-s server] [-p port] -r \"tipo città\"\n", argv[0]);
+        return -1;
+    }
+
+    int i = 1;
+    int r_trovato = 0;
+
+    // CONTROLLO SU OGNI POSSIBILE PARAMETRO MANCANTE//
+
+    while (i < argc)
+    {
+        if (strcmp(argv[i], "-s") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "ERRORE: NON hai inserito il valore dopo -s\n");
+                return -1;
+            }
+            snprintf(IP_SV, sizeof(IP_SV), "%s", argv[i + 1]);
+            i += 2;
+            continue;
+        }
+
+        if (strcmp(argv[i], "-p") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "ERRORE: NON hai inserito la porta dopo -p\n");
+                return -1;
+            }
+            port = atoi(argv[i + 1]);
+            i += 2;
+            continue;
+        }
+
+        if (strcmp(argv[i], "-r") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "ERRORE: NON hai inserito il parametro dopo -r \n");
+                return -1;
+            }
+            if (sscanf(argv[i + 1], "%c %39[^\n]", &wrichiesta.type, wrichiesta.city) != 2)
+            {
+                fprintf(stderr, "ERRORE: Il formato è sbagliato, ricorda che il formato è: \"type\"\n");
+                return -1;
+            }
+            r_trovato = 1;
+            i += 2;
+            continue;
+        }
+        fprintf(stderr, "Il parametro che hai inserito è sconosciuto: %s\n", argv[i]);
+        return -1;
+    }
+    if (!r_trovato)
+    {
+        fprintf(stderr, "ERRORE: Il parametro -r è OBBLIGATORIO!\n");
+        return -1;
+    }
+>>>>>>> Stashed changes
+
+    char IP_SV[64] = DEFAULT_IP_SV;
+    int port = SERVER_PORT;
+    weather_request_t wrichiesta;
+
+    // PER CONTROLLARE CHE I PARAMETRI SIANO ESATTAMENTE 3, ALTRIMENTI, ERRORE //
+    if (argc < 3)
+    {
+        fprintf(stderr, "Il formato per l'utilizzo è: %s [-s server] [-p port] -r \"tipo città\"\n", argv[0]);
+        return -1;
+    }
+
+    int i = 1;
+    int r_trovato = 0;
+
+    // CONTROLLO SU OGNI POSSIBILE PARAMETRO MANCANTE//
+
+    while (i < argc)
+    {
+        if (strcmp(argv[i], "-s") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "ERRORE: NON hai inserito il valore dopo -s\n");
+                return -1;
+            }
+            snprintf(IP_SV, sizeof(IP_SV), "%s", argv[i + 1]);
+            i += 2;
+            continue;
+        }
+
+        if (strcmp(argv[i], "-p") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "ERRORE: NON hai inserito la porta dopo -p\n");
+                return -1;
+            }
+            port = atoi(argv[i + 1]);
+            i += 2;
+            continue;
+        }
+
+        if (strcmp(argv[i], "-r") == 0)
+        {
+            if (i + 1 >= argc)
+            {
+                fprintf(stderr, "ERRORE: NON hai inserito il parametro dopo -r \n");
+                return -1;
+            }
+            if (sscanf(argv[i + 1], "%c %39[^\n]", &wrichiesta.type, wrichiesta.city) != 2)
+            {
+                fprintf(stderr, "ERRORE: Il formato è sbagliato, ricorda che il formato è: \"type\"\n");
+                return -1;
+            }
+            r_trovato = 1;
+            i += 2;
+            continue;
+        }
+        fprintf(stderr, "Il parametro che hai inserito è sconosciuto: %s\n", argv[i]);
+        return -1;
+    }
+    if (!r_trovato)
+    {
+        fprintf(stderr, "ERRORE: Il parametro -r è OBBLIGATORIO!\n");
+        return -1;
+    }
+
+<<<<<<< Updated upstream
+=======
+#if defined WIN32
+    SetConsoleOutputCP(CP_UTF8); // PER GRADI IN TEMPERATURA //
+    WSADATA wsa_data;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+    if (result != NO_ERROR)
+    {
+        printf("Error at WSAStartup()\n");
+        return -1;
+    }
+#endif
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
     // Creazione socket
-    int c_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (c_socket < 0) {
-        perror("Socket creation failed.\n");
+    int my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (my_socket < 0)
+    {
+        errorhandler("Creazione socket fallita\n");
         return -1;
     }
 
-    // Dico l'indirizzo a cui connettersi 
+    // Dico l'indirizzo a cui connettersi
     struct sockaddr_in sad;
     memset(&sad, 0, sizeof(sad));
-    
+
     sad.sin_family = AF_INET;
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     sad.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP del server (localhost)
     sad.sin_port = htons(SERVER_PORT);            // Porta del server (27015)
+=======
+    sad.sin_addr.s_addr = inet_addr(IP_SV); // IP del server (localhost)
+    sad.sin_port = htons(port);             // Porta del server (27015)
+>>>>>>> Stashed changes
+=======
+    sad.sin_addr.s_addr = inet_addr(IP_SV); // IP del server (localhost)
+    sad.sin_port = htons(port);             // Porta del server (27015)
+>>>>>>> Stashed changes
 
     // Connessione al server
-    if (connect(c_socket, (struct sockaddr *)&sad, sizeof(sad)) < 0) {
-        perror("Failed to connect.\n");
-        closesocket(c_socket);
+    if (connect(my_socket, (struct sockaddr *)&sad, sizeof(sad)) < 0)
+    {
+        errorhandler("Errore durante la connessione al server.\n");
+        closesocket(my_socket);
+        clearwinsock();
         return -1;
     }
 
-    printf("Connected to server 127.0.0.1:%d\n", SERVER_PORT);
+    // INVIO PARAMETRI AL SV//
+    if (send(my_socket, (char *)&wrichiesta, sizeof(wrichiesta), 0) != sizeof(wrichiesta))
+    {
+        errorhandler("Send ha inviato un numero differente di bytes di quelli attesi");
+        closesocket(my_socket);
+        clearwinsock();
+        return -1;
+    }
 
-    // TODO: Qui invieresti la weather_request_t con send()
-    // e riceveresti la weather_response_t con recv()
+    // PER RICEVERE I BYTES E VERIFICHE NEL RICEVERE //
 
-    closesocket(c_socket);;
-    printf("Client terminated.\n");
+    int bytes_ricevuti;
+    unsigned long int tot_bytes_ricevuti = 0;
+    char buffer[BUFFER_SIZE];
+    weather_response_t risposta;
+
+    while (tot_bytes_ricevuti < sizeof(risposta))
+    {
+        bytes_ricevuti = recv(
+            my_socket, ((char *)&risposta) + tot_bytes_ricevuti, sizeof(risposta) - tot_bytes_ricevuti, 0);
+
+        if (bytes_ricevuti <= 0)
+        {
+            errorhandler("ERRORE: recv è fallita o la conessione si è persa");
+            closesocket(my_socket);
+            clearwinsock();
+            return -1;
+        }
+        tot_bytes_ricevuti += bytes_ricevuti;
+    }
+
+    // SWITCH PER DARE I RISULTATI IN OUTPUT //
+    switch (risposta.status)
+    {
+    case 1:
+        printf("Ricevuto risulatato dal server con IP %s Città NON DISPONIBILE", IP_SV);
+        break;
+    case 2:
+        printf("Ricevuto risultato dal server con IP %s la richiesta NON è VALIDA", IP_SV);
+        break;
+    case 3:
+        printf("Ricevuto risultato dal server con IP %s per %s: %s \n", IP_SV, wrichiesta.city, risposta_meteo(risposta.type, risposta.value));
+        break;
+    defalut:
+        printf("Risposta del server non comprensibile (STATUS: %d)\n", risposta.status);
+    }
+
+    closesocket(my_socket);
+    clearwinsock();
+    printf("Client terminato");
 
     return 0;
-	
-
-	// TODO: Create socket
-	// my_socket = socket(...);
-
-	// TODO: Configure server address
-	// struct sockaddr_in server_addr;
-	// ...
-
-	// TODO: Connect to server
-	// connect(...);
-
-	// TODO: Implement communication logic
-	// send(...);
-	// recv(...);
-
-	// TODO: Close socket
-	// closesocket(my_socket);
-
-} // main end
+} // finisce main
--- client-project/src/protocol.h
+++ client-project/src/protocol.h
@@ -8,25 +8,43 @@
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // Shared application parameters
 #define SERVER_PORT 27015  // Server port (change if needed)
 #define BUFFER_SIZE 512    // Buffer size for messages
+=======
+=======
+>>>>>>> Stashed changes
+// Parametri condivisi
+#define SERVER_PORT 56700  // REQUISITO: Porta 56700
+#define BUFFER_SIZE 512    // Dimensione Buffer
+#define QUEUE_SIZE 5       // Coda connessioni
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
+// Codici di stato (Status Codes)
+#define VALID_REQ 0     // Successo
+#define INVALID_CITY 1  // Città non disponibile
+#define INVALID_REQ 2   // Richiesta non valida
 
-// Strutture dati applicative (Meteo)
+// Struttura Messaggio di Richiesta (Client -> Server)
 typedef struct {
     char type;        // 't', 'h', 'w', 'p'
-    char city[64];    // Nome città
+    char city[64];    // Nome città (es. "Bari")
 } weather_request_t;
 
+// Struttura Messaggio di Risposta (Server -> Client)
 typedef struct {
-    unsigned int status;  // Codice stato
-    char type;            // Echo del tipo richiesta
-    float value;          // Valore meteo
+    unsigned int status;  // 0, 1, 2
+    char type;            // Echo del tipo
+    float value;          // Valore numerico
 } weather_response_t;
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // Prototipi funzioni meteo (se servono al server)
 float get_temperature(void);    // Range: -10.0 to 40.0 °C
 float get_humidity(void);       // Range: 20.0 to 100.0 %
@@ -37,5 +55,17 @@ float get_pressure(void);       // Range: 950.0 to 1050.0 hPa
  * Example function to implement:
  * int connect_to_server(const char* server_address);
  */
+=======
+=======
+>>>>>>> Stashed changes
+// Prototipi funzioni meteo (utili per il server)
+float get_temperature(void);
+float get_humidity(void);
+float get_wind(void);
+float get_pressure(void);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 
 #endif /* PROTOCOL_H_ */
